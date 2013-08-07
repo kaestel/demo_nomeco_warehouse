85ca9b803efbefc40072928597914c624ed383a4
kaestel <martin@hvadhedderde.com>
2013-08-07 19:36:53 +0200

    refs

--- src/www/js/lib/seg_desktop_include.js
+++ src/www/js/lib/seg_desktop_include.js
@@ -1,28 +1,12 @@
+document.write('<script type="text/javascript" src="/js/jes/src/u.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-debug.js"></script>');
 
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/framework/u.js"></script>');
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/framework/u-debug.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-dom.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-events.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-xmlrequest.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-timer.js"></script>');
 
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/framework/u-dom.js"></script>');
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/framework/u-events.js"></script>');
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/framework/u-xmlrequest.js"></script>');
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/framework/u-timer.js"></script>');
-
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/framework/u-init.js"></script>');
-
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/lib/i-content.js"></script>');
-//document.write('<script type="text/javascript" src="js/lib/i-content.js"></script>');
-document.write('<script type="text/javascript" src="http://lifted.wires.dk/js/lib/i-carousel.js"></script>');
-
-/*
-document.write('<script type="text/javascript" src="/js/framework/u.js"></script>');
-document.write('<script type="text/javascript" src="/js/framework/u-debug.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/framework/u-dom.js"></script>');
-document.write('<script type="text/javascript" src="/js/framework/u-events.js"></script>');
-document.write('<script type="text/javascript" src="/js/framework/u-xmlrequest.js"></script>');
-document.write('<script type="text/javascript" src="/js/framework/u-timer.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/framework/u-init.js"></script>');
+document.write('<script type="text/javascript" src="/js/jes/src/u-init.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/i-content.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/i-carousel.js"></script>');*/
+document.write('<script type="text/javascript" src="/js/lib/i-carousel.js"></script>');
